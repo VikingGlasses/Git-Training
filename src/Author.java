e84bf4a319404039858e08810f5438615748c11e
Harald Billstein <harald.billstein@gmail.com>
2016-11-03 11:54:19 +0100

    project added

---
+++ src/Author.java
@@ -0,0 +1,36 @@
+/**
+ *
+ * @author Harald
+ */
+public class Author {
+
+    private String name;
+    private String email;
+    private char gender;
+
+    public Author(String name, String email, char gender) {
+        this.name = name;
+        this.email = email;
+        this.gender = gender;
+    }
+    
+    public String getName(){
+        return this.name;
+    }
+    
+    public String getEmail(){
+        return this.email;
+    }
+    
+    public void setEmail(String email){
+        this.email = email;
+    }
+    
+    public char getGender(){
+        return this.gender;
+    }
+    public String toString(){
+        return "Name: " + this.name + "\n Email: " + this.email + "\n Gender: " + this.gender;
+    }
+
+}
