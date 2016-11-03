d1bd537389753afd4ff1f72527f1870650e28bf3
Harald Billstein <harald.billstein@gmail.com>
2016-11-03 14:32:56 +0100

    Added file

---
+++ src/Book.java
@@ -0,0 +1,55 @@
+
+/**
+ *
+ * @author Harald
+ */
+public class Book {
+
+    private String name;
+    private Author author;
+    private double price;
+    private int qty;
+
+    public Book(String name, Author author, double price) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Book(String name, Author author, double price, int qty) {
+        this.name = name;
+        this.author = author;
+        this.price = price;
+        this.qty = qty;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public int getQty() {
+        return this.qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public String toString(Author author) {
+        return "Book: " + this.name + "Author: " + author
+                + "Price: " + this.getPrice() + "qty: " + this.getQty();
+    }
+
+}
