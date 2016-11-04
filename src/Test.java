be18583234b1b00a56ce74cd23b39d73e0b102c9
Harald Billstein <harald.billstein@gmail.com>
2016-11-04 15:08:08 +0100

    added MyCircle

--- src/Test.java
+++ src/Test.java
@@ -11,29 +11,10 @@
 public class Test {
 
     public static void main(String[] args) {
-
-        Author author1 = new Author("Harald Billstein", "harald@epost.com", 'M');
-        Author author2 = new Author("Lisette Billstein", "Lisette@epost.com", 'F');
-        Author author3 = new Author("Seth Billstein", "Seth@epost.com", 'M');
-
-        Author[] author = new Author[3];
-        author[0] = author1;
-        author[1] = author2;
-        author[2] = author3;
-
-        Book minBok = new Book("Hemma hos röran!", author, 120);
-        minBok.setQty(7);
-
-        System.out.println(minBok);
-        MyPoint[] myPoints = new MyPoint[10];
-        for (int i = 0; i < 10; ++i) {
-            myPoints[i] = new MyPoint(i+1,i+1);
-        }
         
-        for (int i =0; i < myPoints.length;i++){
-            System.out.println(myPoints[i]);
-        }
-        
+        MyPoint point = new MyPoint(3,4);
+        MyCircle circle = new MyCircle(point,4);
+        circle.
 
     }
 
