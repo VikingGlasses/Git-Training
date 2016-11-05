701bb455036375e85db09338031276789506f8bd
Harald Billstein <harald.billstein@gmail.com>
2016-11-05 14:01:03 +0100

    Finished MyRectangle and updated main(test) to try it out

--- src/Test.java
+++ src/Test.java
@@ -12,12 +12,11 @@ public class Test {
 
     public static void main(String[] args) {
 
+        MyPoint p1 = new MyPoint(0,0);
+        MyPoint p2 = new MyPoint(2,2);
+        MyRectangle rec = new MyRectangle(p1,p2);
         
-        
-        MyTriangle tri = new MyTriangle(1,2,1,2,1,2);
-        
-        System.out.println(tri.getType());
-        
+        System.out.println(rec.getPerimiter());
         
 
     }
