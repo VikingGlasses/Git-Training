1231a9c6a06a9524ae5bff9d5e26fa92e05bf8e4
Harald Billstein <harald.billstein@gmail.com>
2016-11-05 15:06:24 +0100

    Finished Customer class and started working on Invoiuce class

--- src/Invoice.java
+++ src/Invoice.java
@@ -1,13 +1,41 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Harald
- */
 public class Invoice {
+    private final int ID;
+    private Customer customer;
+    private double amount;
+    
+    public Invoice(int ID, Customer customer, double amount){
+        this.ID = ID;
+        this.customer = customer;
+        this.amount = amount;
+    }
+    
+    public int getID(){
+        return this.ID;
+    }
+    
+    public Customer getCustomer(){
+        return this.customer;
+    }
+    
+    public void setCostumer(Customer customer){
+        this.customer = customer;
+    }
+    
+    public double getAmount(){
+        return this.amount;
+    }
+    
+    public void setAmount(double amount){
+        this.amount = amount;
+    }
+    
+    public String getCustomerName(){
+        return this.customer.getName();
+    }
+    
+    public double getAmountAfterDiscount(){
+        return 2; //todo
+    }
     
 }
