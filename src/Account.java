5fb8eb0d745d6628a058abc926d3569516d7a835
Harald Billstein <harald.billstein@gmail.com>
2016-11-06 13:20:33 +0100

    Finished Accoutnclass

--- src/Account.java
+++ src/Account.java
@@ -1,39 +1,63 @@
 
+import static java.lang.Math.round;
+
 public class Account {
-    private final  int ID;
+
+    private final int ID;
     private Customer customer;
-    private double  balance;
-    
-    public Account(int id, Customer customer, double balance){
+    private double balance;
+
+    public Account(int id, Customer customer, double balance) {
         this.ID = id;
         this.customer = customer;
         this.balance = balance;
     }
-    
-    public Account(int id, Customer customer){
+
+    public Account(int id, Customer customer) {
         this.ID = id;
         this.customer = customer;
     }
-    
-    public int getID(){
+
+    public int getID() {
         return this.ID;
     }
-    
-    public Customer getCustomer(){
+
+    public Customer getCustomer() {
         return this.customer;
     }
-    
-    public double getBalance(){
+
+    public double getBalance() {
         return this.balance;
     }
-    
-    public void setBalance(double balance){
+
+    public void setBalance(double balance) {
         this.balance = balance;
     }
-    
-    public String toString(){
-        return "Name; " + customer.toString() + "\nBalance: " + this.balance; 
+
+    public String toString() {
+        return "Name; " + customer.toString()
+                + "\nBalance: " + round(this.balance);
     }
-    
-    
+
+    public String getCustomerName() {
+        return customer.getName();
+    }
+
+    public Account deposit(double amount) {
+        Account out = new Account(this.ID, this.customer);
+        out.balance = this.balance + amount;
+        return out;
+    }
+
+    public Account withraw(double amount) {
+        Account out = new Account(this.ID, this.customer);
+        if (this.balance >= amount){
+        out.setBalance(this.balance - amount);
+        } else {
+            out.setBalance(balance);
+            System.out.println("amount withrawn exceeds the current balance!");
+        }
+        return out;
+    }
+
 }
--- src/Test.java
+++ src/Test.java
@@ -11,10 +11,15 @@
 public class Test {
 
     public static void main(String[] args) {
-        Customer c1 = new Customer(001,"harald",10);
-        Invoice inv = new Invoice(001,c1,100);
+        Customer myCustomer = new Customer(007,"Bond",50);
+        Account myAc = new Account(001,myCustomer,1000);
         
-        System.out.println(inv.getAmountAfterDiscount());
+        
+        myAc = myAc.withraw(1200);
+        
+        
+        System.out.println(myAc);
+        
 
     }
 
